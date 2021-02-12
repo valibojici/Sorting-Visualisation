2b86c4f7929aa5a662adc6328bfc40a549bd14d5
Vali Bojici <valibojici@gmail.com>
2021-02-12 12:52:30 +0200

    added radixsort and colors to other generators

--- sorting.py
+++ sorting.py
@@ -10,9 +10,9 @@ def bubblesort_gen(columns,lo,hi):
             if columns[i] < columns[i-1]:
                 columns[i].swap_value(columns[i-1])
                 ok = False
-                columns[i].highlight = True
+                columns[i].color = (0,255,0)
                 yield columns
-                columns[i].highlight = False
+                columns[i].color = (255,255,255)
 
 def selectionsort_gen(columns,lo,hi):
     '''Generator for selectionsort'''
@@ -41,33 +41,50 @@ def insertionsort_gen(columns,lo,hi):
     '''Generator for insertionsort'''
     for i in range(lo+1,hi+1):
         x = copy.deepcopy(columns[i])
+        columns[i].color = (0,255,0)
+        yield columns
+        columns[i].color = (255,255,255)
         j = i - 1
         while j >= lo and columns[j] > x:
             columns[j+1].value = columns[j].value
-            j -= 1
+
+            columns[j+1].color = (255, 230, 51)
             yield columns
+            columns[j+1].color = (255,255,255)
+
+            j -= 1
         columns[j+1] = x
+        columns[j+1].color = (0,255,0)
         yield columns
+        columns[j+1].color = (255,255,255)
 
 def quicksort_gen(columns, lo, hi):
     '''Generator for quicksort'''
-    def partition(columns, lo, hi):
+    def partition(columns, lo, hi,*, pivot=False):
+        L,H = lo,hi
+        columns[lo].color = columns[hi].color = (255, 230, 51)
         d = 0
         while lo < hi:
             if columns[lo] > columns[hi]:
                 columns[lo].swap_value(columns[hi])
-                yield columns
+                if not pivot:
+                    yield columns
                 d = 1 - d
             lo += d
             hi -= 1 - d
-        yield lo
+        if pivot:
+            yield lo
+        columns[L].color = columns[H].color =  (255,255,255)
 
     if lo < hi:
-        for state in partition(columns, lo, hi):
-            if isinstance(state,list):
-                yield state
-            else:
-                p = state
+        columns_2 = copy.deepcopy(columns)
+        p = partition(columns_2,lo,hi,pivot=True)
+        p = next(p)
+        columns[p].color =  (255, 0, 0)
+        for state in partition(columns,lo,hi,pivot=False):
+            yield state
+        columns[p].color = (255,255,255)
+
         for state in quicksort_gen(columns,lo,p-1):
             yield state
         for state in quicksort_gen(columns,p+1,hi):
@@ -99,7 +116,7 @@ def mergesort_gen(columns,lo,hi):
 
 def heapsort_gen(columns,lo,hi):
     def heap_down(idx,hi):
-       
+        
         def idx_child_max(idx,hi):
             if idx * 2 + 2 <= hi:
                 return idx * 2 + 1 if columns[idx*2+1] > columns[idx*2+2] else idx * 2 + 2
@@ -107,30 +124,62 @@ def heapsort_gen(columns,lo,hi):
 
         if idx <= (hi-1) // 2:
             idx_child = idx_child_max(idx,hi)
+            child_color = columns[idx_child].color
+
+            columns[idx].color = (255,0,0)
+            columns[idx_child].color = (255,150,150)
+            yield columns
+            columns[idx].color = (255,255,255)
+            columns[idx_child].color = child_color
+
             if columns[idx] < columns[idx_child]:
                 columns[idx].swap_value(columns[idx_child])
+                
+                columns[idx_child].color = (255,0,0)
                 yield columns
+                columns[idx_child].color = child_color
+
                 for state in heap_down(idx_child,hi):
                     yield state
 
     def heapify():
+        # leaves
+        for i in range((hi-1)//2 + 1, hi+1):
+            columns[i].color = (143, 132, 209)
+        yield columns
+
         for i in range(len(columns)//2 - 1,-1,-1):
             for state in heap_down(i,hi):
                 yield state
 
     for state in heapify():
         yield state
-    for i in range(hi,lo-1,-1):
-        columns[lo].swap_value(columns[i])
+    
+    for i in range(hi,lo,-1):
+        columns[lo].color = (100,200,100)
+        yield columns
+        columns[lo],columns[i] = columns[i],columns[lo]
+        yield columns
+        columns[lo].color = (255,0,0)
+        yield columns
+
+        # leaves
+        for k in range((i-2)//2 + 1, i):
+            columns[k].color = (143, 132, 209)
+        yield columns
+
         for state in heap_down(lo,i-1):
             yield state
-    yield columns
+
+    for i in range(lo,hi+1):
+        columns[i].color = (255,255,255)
+        yield columns
 
 
-def radixsort_gen(columns,lo,hi):
+def radixsort64_gen(columns,lo,hi):
     for col in columns:
         col.value = round(col.value)
-    POWER = 4
+    POWER = 6
     BASE = 2 ** POWER
     count = []
     output = [0 for _ in range(len(columns))]
@@ -156,11 +205,39 @@ def radixsort_gen(columns,lo,hi):
         steps += POWER
 
 
+def radixsort10_gen(columns,lo,hi):
+    for col in columns:
+        col.value = round(col.value)
+    BASE = 10
+    count = []
+    output = [0 for _ in range(len(columns))]
+
+    for k in range(0,max([len(str(x.value)) for x in columns])):
+        count = [0 for _ in range(BASE)]
+
+        for i in range(lo,hi+1):
+            count[(columns[i].value // (10 ** k) ) % BASE] += 1
+
+        for i in range(1,BASE):
+            count[i] += count[i-1]
+
+        for i in range(hi,lo-1,-1):
+            index = (columns[i].value // (10 ** k) ) % BASE
+            output[count[index]-1] = columns[i].value
+            count[index] -= 1
+
+        for i in range(lo,hi+1):
+            columns[i].value = output[i]
+            yield columns
+
+
+
 sort_gen = [bubblesort_gen,
             selectionsort_gen,
             insertionsort_gen,
             quicksort_gen,
             mergesort_gen,
             heapsort_gen,
-            radixsort_gen
+            radixsort64_gen,
+            radixsort10_gen
 ]
